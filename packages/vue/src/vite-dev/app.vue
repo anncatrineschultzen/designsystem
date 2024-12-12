35384ba49fa55b14b23041edfc6b7c735ab0a7eb
anncatrineschultzen <151722072+anncatrineschultzen@users.noreply.github.com>
2024-12-12 08:02:36 +0100

    Update app.vue

--- packages/vue/src/vite-dev/app.vue
+++ packages/vue/src/vite-dev/app.vue
@@ -1,20 +1,58 @@
 <template>
     <div>
-        <f-text-field v-model="namn" v-validation.required maxlength="100">Namn</f-text-field>
-        <pre>Hej {{ namn }}</pre>
+        <f-wizard v-model="current" header-tag="h2" @completed="onCompleted">
+            <f-wizard-step key="foo" :use-error-list="false" title="Stegrubrik 1">
+                <f-datepicker-field v-model="date" v-validation.required> Etikett-rubrik </f-datepicker-field>
+                <f-text-field v-validation.maxLength="{ maxLength: { length: 100 } }">
+                    Etikett-rubrik (frivillig)
+                </f-text-field>
+            </f-wizard-step>
+
+            <f-wizard-step key="bar" :use-error-list="false" title="Stegrubrik 2">
+                <f-datepicker-field v-model="date" v-validation.required> Etikett-rubrik </f-datepicker-field>
+                <f-text-field v-validation.maxLength="{ maxLength: { length: 100 } }">
+                    Etikett-rubrik (frivillig)
+                </f-text-field>
+            </f-wizard-step>
+            <f-wizard-step key="baz" :use-error-list="false" title="Stegrubrik 3">
+                <f-datepicker-field v-model="date" v-validation.required> Etikett-rubrik </f-datepicker-field>
+                <f-text-field v-validation.maxLength="{ maxLength: { length: 100 } }">
+                    Etikett-rubrik (frivillig)
+                </f-text-field>
+                <template #next-button-text> Klar </template>
+            </f-wizard-step>
+            <f-wizard-step key="bo" :use-error-list="false" title="Stegrubrik 4">
+                <f-datepicker-field v-model="date" v-validation.required> Etikett-rubrik </f-datepicker-field>
+                <f-text-field v-validation.maxLength="{ maxLength: { length: 100 } }">
+                    Etikett-rubrik (frivillig)
+                </f-text-field>
+                <template #next-button-text> Klar </template>
+            </f-wizard-step>
+        </f-wizard>
+        <p v-if="done">Allt är ifyllt</p>
     </div>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from "vue";
-import { FTextField } from "../components";
+import { FWizard, FWizardStep, FTextField, FDatepickerField } from "../components";
 
 export default defineComponent({
-    components: { FTextField },
+    name: "WizardExampleDefault",
+    components: { FWizard, FWizardStep, FTextField, FDatepickerField },
     data() {
         return {
-            namn: "",
+            current: null,
+            visible: true,
+            done: false,
+            date: "",
+            options: "",
         };
+    },
+    methods: {
+        onCompleted() {
+            this.done = true;
+        },
     },
 });
 </script>
